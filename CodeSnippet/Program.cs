913ad2f76917fce9e7dab4d83b1f997196865fb5
Attila Szigeti <attila.szigeti@outlook.com>
2017-03-20 19:34:28 +0100

    first commit

---
+++ CodeSnippet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnippet
+{
+    class Program
+    {
+        [DataMember]
+        public string MyProperty { get; set; }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
